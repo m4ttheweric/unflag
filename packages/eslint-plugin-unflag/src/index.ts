2e6558f7402009bb6541459d38934c9e120202a6
Matthew Goodwin <goodwin.matthew.eric@gmail.com>
2026-08-14 21:47:24 -0500

    chore(eslint-plugin): 0.2.0 -- require-unready-for-deferred-reads

--- packages/eslint-plugin-unflag/src/index.ts
+++ packages/eslint-plugin-unflag/src/index.ts
@@ -2,7 +2,7 @@ import noRawConfigReads from './rules/no-raw-config-reads';
 import requireUnreadyForDeferredReads from './rules/require-unready-for-deferred-reads';
 
 const plugin = {
-  meta: { name: '@m4ttheweric/eslint-plugin-unflag', version: '0.1.0' },
+  meta: { name: '@m4ttheweric/eslint-plugin-unflag', version: '0.2.0' },
   rules: {
     'no-raw-config-reads': noRawConfigReads,
     'require-unready-for-deferred-reads': requireUnreadyForDeferredReads,
